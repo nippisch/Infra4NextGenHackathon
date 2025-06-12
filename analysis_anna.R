dcf941c13ac4f5bb8d84bfbbc573e2179ade495e
Anna Fuchs <152496556+fuchsanna@users.noreply.github.com>
2025-06-12 18:18:01 +0200

    update code for figure 2 and 3

--- analysis_anna.R
+++ analysis_anna.R
@@ -1,1 +1,155 @@
 # analysis Anna
+
+library(readr)
+library(survey)
+library(srvyr)
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(showtext)
+library(ggrounded)
+
+font_add_google("Montserrat", "montserrat")
+
+relevant <- read_rds(file = "data/relevant_recoded.R")
+
+# Figure 2 ----
+svy_dat <- relevant %>%
+  filter(!is.na(w2eq13)) %>% 
+  as_survey_design(weights = w2weight) # declare survey design
+
+svy_summary <- svy_dat %>% # compute weighted counts for each category of w2eq13
+  group_by(w2eq13) %>%
+  summarise(
+    weighted_n = survey_total(),             # weighted count
+    pct        = survey_mean(vartype = "ci") # weighted proportion + CI
+  )
+
+# plot of weighted counts
+ggplot(svy_summary, aes(x = w2eq13, y = weighted_n)) +
+  geom_col() +
+  labs(
+    x = "w2eq13 response",
+    y = "Weighted count",
+    title = "Weighted Distribution of w2eq13 (Wave 2)"
+  )
+
+
+# percentage plot
+svy_pct <- svy_dat %>%
+  group_by(w2eq13) %>%
+  summarise(
+    wt_n     = survey_total(vartype = "ci"),    # weighted count + CI
+  ) %>%
+  ungroup() %>%
+  mutate(
+    pct      = wt_n / sum(wt_n),                # proportion
+    pct_low  = wt_n_low  / sum(wt_n),           # lower CI on proportion
+    pct_upp  = wt_n_upp  / sum(wt_n)            # upper CI on proportion
+  )
+
+ggplot(svy_pct, aes(x = w2eq13, y = pct)) +
+  geom_col_rounded(fill = "#AB110F") +
+  geom_text(aes(label = paste(round(pct * 100, 0), "%")), position = position_dodge(0.9), vjust = 2, size = 4, color = "white") +
+  # geom_errorbar(aes(ymin = pct_low, ymax = pct_upp), width = 0.2) +
+  
+  scale_y_continuous(labels = percent_format(accuracy = 1),
+                     limits = c(0, 0.4),
+                     breaks = seq(0, 0.4, by = 0.1),
+                     expand = c(0,0)) +
+  scale_x_continuous(
+    breaks = 1:5,
+    labels = c(
+      "Strongly agree",
+      "Agree",
+      "Neither agree\nnor disagree",
+      "Disagree",
+      "Strongly disagree"
+    )
+  ) +
+  labs(
+    #x     = "The state should increase the spending on social assistance on the poor",
+    x = "",
+    y     = "% of respondents"
+  ) +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = '#FFF7F5', colour = '#FFF7F5'),
+        plot.background = element_rect(fill = '#FFF7F5', colour = '#FFF7F5'),
+        axis.text.x = element_text(colour="#22444b"),
+        axis.text.y = element_text(colour="#22444b"))
+
+# Figure 3
+## Binary Regression with weighted data
+relevant    <- read_rds("data/relevant_recoded.R")
+
+countries   <- c("AT","BE","CZ","FI","FR","GB","HU","IS","PL","PT","SI")
+
+relevant_bin <- relevant %>%
+  filter(w2eq13 != 3, w2eq1 != 3) %>%
+  mutate(
+    policy_bin = ifelse(w2eq13 %in% c(1,2), 1L, 0L),
+    income_bin = ifelse(w2eq1   %in% c(4,5), 1L, 0L)
+  ) %>%
+  filter(!is.na(policy_bin), !is.na(income_bin), cntry %in% countries)
+
+nested <- relevant_bin %>%
+  group_by(cntry) %>%
+  nest()
+
+results_svy <- nested %>%
+  mutate(
+    design = map(data,    ~ as_survey_design(.x, weights = w2weight)),
+    model  = map(design,  ~ svyglm(policy_bin ~ income_bin,
+                                   design = .x,
+                                   family = quasibinomial())),
+    tidy   = map(model,   ~ broom::tidy(.x) %>% filter(term == "income_bin"))
+  ) %>%
+  select(cntry, tidy) %>%
+  unnest(tidy) %>%
+  transmute(
+    cntry,
+    beta = estimate,
+    sd   = std.error,
+    p    = p.value,
+    sig  = ifelse(p < 0.05, 1L, 0L)
+  )
+
+
+results_svy %>%
+  ggplot(aes(x = beta, y = reorder(cntry, beta))) +
+  geom_point(colour = "#AB110F", size = 3) +
+  geom_errorbarh(aes(
+    xmin = beta - 1.96 * sd,
+    xmax = beta + 1.96 * sd
+  ), height = 0.3, colour = "#AB110F", size = 1) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(
+    x     = "",
+    y = "",
+    title = ""
+  ) +
+  scale_x_continuous(limits = c(-1.5, 2.5),
+                     breaks = seq(-1.5, 2.5, by = 1),
+                     expand = c(0,0.1)) +
+  scale_y_discrete(
+    labels = c("SI - Slovenia",
+               "AT - Austria",
+               "IS - Iceland",
+               "FI - Finland",
+               "GB - United Kingdom",
+               "FR - France",
+               "PL - Poland",
+               "CZ - Czechia",
+               "BE - Belgium",
+               "HU - Hungary",
+               "PT - Portugal"
+               
+    )
+  ) +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = '#FFF7F5', colour = '#FFF7F5'),
+        plot.background = element_rect(fill = '#FFF7F5', colour = '#FFF7F5'),
+        axis.text.x = element_text(colour="#22444b"),
+        axis.text.y = element_text(colour="#22444b"))
